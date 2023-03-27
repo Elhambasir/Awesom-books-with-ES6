c1274bf69f5fc3446407214de4c550c69cb15acf
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-03-27 21:26:10 +0430

    fix eslint error third attempt

--- modules/element.js
+++ modules/element.js
@@ -1,24 +1,23 @@
 export default class elements {
-
-    bookList = document.querySelector('.list');
-
-    addToList = document.querySelector('.addToList');
-
-    contact = document.querySelector('.contact');
-    
-    btnList = document.querySelector('#list');
-    
-    btnAddNew = document.querySelector('#addNew');
-    
-    btnContact = document.querySelector('#contact');
-    
-    dateTimeContainer = document.querySelector('#date');
-    
-    titleInput = document.querySelector('.bookTitle');
-    
-    authorInput = document.querySelector('.author');
-    
-    addBtn = document.querySelector('.btnAdd');
-    
-    removeButton = document.querySelector('#rm');
+	bookList = document.querySelector('.list');
+	
+	addToList = document.querySelector('.addToList');
+	
+	contact = document.querySelector('.contact');
+	
+	btnList = document.querySelector('#list');
+	
+	btnAddNew = document.querySelector('#addNew');
+	
+	btnContact = document.querySelector('#contact');
+	
+	dateTimeContainer = document.querySelector('#date');
+	
+	titleInput = document.querySelector('.bookTitle');
+	
+	authorInput = document.querySelector('.author');
+	
+	addBtn = document.querySelector('.btnAdd');
+	
+	removeButton = document.querySelector('#rm');
 }
